5b8022359ab74c68ab6a465ecef66d5b46dc9ac1
Pranav1703 <116703760+Pranav1703@users.noreply.github.com>
2023-07-14 17:32:59 +0530

    Create binarySearch.js

---
+++ searching/binarySearch.js
@@ -0,0 +1,19 @@
+function binarySearch(arr,key){
+    let start = 0;
+    let end = arr.length-1;
+    while(start <= end){
+        let mid = Math.floor((start+end)/2);
+        
+        if(arr[mid]===key){
+            return mid;
+        }
+        else if(arr[mid]>key){
+            end = mid-1;
+        }
+        else{
+            start = mid +1;
+        }
+        
+    }
+    return -1;
+}
