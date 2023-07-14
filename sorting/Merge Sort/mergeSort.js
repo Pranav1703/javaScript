263b1606c5e38e45119a55c6e683fb08fc7ae95e
Pranav1703 <116703760+Pranav1703@users.noreply.github.com>
2023-07-14 21:55:37 +0530

    Create mergeSort.js

---
+++ sorting/Merge Sort/mergeSort.js
@@ -0,0 +1,35 @@
+function merge(arr1,arr2){
+    let res = [];
+    let i =0;
+    let j=0;
+    let k = 0;
+
+    while(i<arr1.length && j<arr2.length){
+        if(arr1[i]>arr2[j]){
+            res[k] = arr[j];
+            j++;
+        }
+        else{
+            res[k] = arr[i];
+            i++;
+        }
+        k++;
+    }
+    while(i<arr1.length){
+        res[k] = arr[i];
+        i++;
+        k++;
+    }
+    while(j<arr2.length){
+        res[k] = arr[j];
+        j++;
+        k++;
+    }
+    
+}
+
+function mergeSort(arr){
+    if(arr.length <= 1){
+        return arr;
+    }
+}
