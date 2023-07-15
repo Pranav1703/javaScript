268e300e6d2b5dd139db6799afdc0210d1072bc7
Pranav1703 <116703760+Pranav1703@users.noreply.github.com>
2023-07-15 18:25:08 +0530

    Update pivothelper.js

--- sorting/quick sort/pivothelper.js
+++ sorting/quick sort/pivothelper.js
@@ -3,7 +3,7 @@ function swap(arr,i,j){
     arr[i] = arr[j];
     arr[j] = temp;
 }
-function pivotHelper(arr,start =0,end = arr.length+1){
+function pivotHelper(arr,start =0,end = arr.length-1){
     let pivot = arr[start];
     let swapIndex = start;
     for(let i=0;i<arr.length;i++){
