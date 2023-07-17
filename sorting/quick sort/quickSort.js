c509e54cf223d7c18fdc388405f8b02ea5dcd5a6
Pranav1703 <116703760+Pranav1703@users.noreply.github.com>
2023-07-17 12:24:41 +0530

    Update quickSort.js

--- sorting/quick sort/quickSort.js
+++ sorting/quick sort/quickSort.js
@@ -1,1 +1,26 @@
+function swap(arr,i,j){
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+function pivotHelper(arr,start =0,end = arr.length-1){
+    let pivot = arr[start];
+    let swapIndex = start;
+    for(let i=0;i<=end;i++){
+        if(pivot>arr[i]){
+            swapIndex++;
+            swap(arr,swapIndex,i)
+        }
+    }
+    swap(arr,start,swapIndex);
+    return swapIndex;
+}
 
+function quickSort(arr,left = 0,right = arr.length - 1){
+    if(left<right){
+        let pivotIndex = pivotHelper(arr,left,right);
+        quickSort(arr,left,pivotIndex-1);
+        quicksort(arr,pivotIndex+1,right);
+    }
+    return arr;
+}
